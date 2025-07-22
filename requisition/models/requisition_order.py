65c5b9ddca8a737ce49ae440d28fbe19c26ceb41
Courage19 <gumberocourage19@gmail.com>
2025-07-22 09:21:56 +0200

    defined requisition type

--- requisition/models/requisition_order.py
+++ requisition/models/requisition_order.py
@@ -20,6 +20,7 @@ class RequisitionOrder(models.Model):
     uom = fields.Char(related='product_id.uom_id.name',
                      string='Unit of Measure', help='Product unit of measure')
     requisition_type= fields.Text(string='Requisition Type')
+    partner_id = fields.Text(string='Partner ID')
 
     @api.depends('product_id')
     def _compute_name(self):
